7cc10cc09c1c585f15c45753c55d78806a6b94b9
Masl <marcel.z2006@gmail.com>
2024-04-28 03:04:32 +0200

    more design stuff

--- client/indexConn.js
+++ client/indexConn.js
@@ -24,9 +24,13 @@ function createServerEntry(imgSrc, serverId, serverName) {
     let img = document.createElement("img");
     img.src = imgSrc;
     img.className = "sidebar-entry";
-    img.onmouseover = () => {serverHoverStart(img, serverName)};
+    img.onmouseover = () => {
+        serverHoverStart(img, serverName)
+    };
     img.onmouseleave = serverHoverEnd;
-    img.onclick = () => {serverClicked(img, serverId)};
+    img.onclick = () => {
+        serverClicked(img, serverId)
+    };
     li.appendChild(img);
     docServerList.appendChild(li);
 }
@@ -36,7 +40,7 @@ function createServerList() {
     docLastServerEntry = null;
 
     createServerEntry("./assets/imgs/dm.png", -1, `DMs`);
-    for (let i = 0; i < 10; i++)
+    for (let i = 0; i < 20; i++)
         createServerEntry("./assets/imgs/uh.png", i, `S: ${i}`);
 }
 
@@ -50,42 +54,91 @@ function createChannelEntry(channelId, channelName) {
     let span = document.createElement("span");
     span.className = "chat-selector-entry";
     span.textContent = channelName;
-    span.onclick = () => {channelClicked(span, channelId)};
+    span.onclick = () => {
+        channelClicked(span, channelId)
+    };
     li.appendChild(span);
     docChannelList.appendChild(li);
 }
 
-function createChannelList() {
+function createChannelList(serverId) {
     docChannelList.innerHTML = "";
     docLastChannelEntry = null;
 
     createChannelEntry(0, "General");
-    for (let i = 0; i < 10; i++)
+    for (let i = 0; i < 40; i++)
         createChannelEntry(i, `Channel ${i}`);
 }
 
-function serverClicked(element, serverId)
+const docChatList = document.getElementById("main-chat-content-list");
+// <li><div class="chat-entry"><span class="chat-entry-username">Username</span> at <span>TIME</span><br><p class="chat-entry-message">Message yes es</p></div></li>
+function createChatEntry(username, time, message)
 {
+    let li = document.createElement("li");
+    let div = document.createElement("div");
+    div.className = "chat-entry";
+    let span1 = document.createElement("span");
+    span1.className = "chat-entry-username";
+    span1.textContent = username;
+    let span2 = document.createElement("span");
+
+    let utcDate = new Date(time);
+    let localDate = new Date(utcDate.getTime() - utcDate.getTimezoneOffset() * 60000);
+    time = localDate.toISOString().slice(0, 19).replace("T", " ");
+
+    span2.textContent = time;
+    let br = document.createElement("br");
+    let p = document.createElement("p");
+    p.className = "chat-entry-message";
+    p.textContent = message;
+    div.appendChild(span1);
+    div.appendChild(document.createTextNode(" at "));
+    div.appendChild(span2);
+    div.appendChild(br);
+    div.appendChild(p);
+    li.appendChild(div);
+    docChatList.appendChild(li);
+}
+
+function createChatList(channelId) {
+    docChatList.innerHTML = "";
+
+    for (let i = 0; i < 30; i++)
+        createChatEntry(`User ${i}`, "2024-04-27T17:43:03.164Z", `Message ${i}`);
+}
+
+function serverClicked(element, serverId) {
     console.log(`Server ${serverId} clicked`);
     if (docLastServerEntry)
         docLastServerEntry.classList.remove("sidebar-entry-active");
     element.classList.add("sidebar-entry-active");
     docLastServerEntry = element;
+
+    createChannelList(serverId);
 }
 
-function channelClicked(element, channelId)
-{
+function channelClicked(element, channelId) {
     console.log(`Channel ${channelId} clicked`);
     if (docLastChannelEntry)
         docLastChannelEntry.classList.remove("chat-selector-entry-active");
     element.classList.add("chat-selector-entry-active");
     docLastChannelEntry = element;
+
+    createChatList(channelId);
 }
 
 async function doConnInit() {
     createServerList();
 
     createChannelList();
+
+    createChatList();
 }
 
 doConnInit().then();
+
+
+function setChatInfoVisibility(visible)
+{
+    document.documentElement.style.setProperty("--main-chat-show-info", visible ? "1" : "0");
+}
