6408e6ab1ccfd4dc9bef66033c92bc829ba4bdff
Masl <marcel.z2006@gmail.com>
2024-08-14 02:35:38 +0200

    doing some cursed stuff but now images show up and files can be downloaded!

--- client/lib/other/markedExtension.js
+++ client/lib/other/markedExtension.js
@@ -1,3 +1,4 @@
+
 function waitForElm(selector) {
     return new Promise(resolve => {
         if (document.querySelector(selector)) {
@@ -19,6 +20,15 @@ function waitForElm(selector) {
     });
 }
 
+const doesImageExist = (url) =>
+    new Promise((resolve) => {
+        const img = new Image();
+
+        img.src = url;
+        img.onload = () => resolve(true);
+        img.onerror = () => resolve(false);
+    });
+
 // Override function
 const renderer = {
     image(token) {
@@ -31,15 +41,47 @@ const renderer = {
             let randomInt = getRandomIntInclusive(100000, 9999999);
             let imgId = `img-${fileId}-${randomInt}`;
             waitForElm(`#${imgId}`).then(async (element) => {
-                let img = await internalGetFile(currentUser['mainAccount'], getCurrentChatUserId(), parseInt(fileId));
-                console.log(img);
-                let fileData = img["data"]; // is a uint8array
-                let filename = img["filename"];
-                let blob = new Blob([fileData], {type: "image/png"});
+                let file = await internalGetFile(currentUser['mainAccount'], getCurrentChatUserId(), parseInt(fileId));
+                console.log(file);
+                let fileData = file["data"]; // is a uint8array
+                let filename = file["filename"];
+
+                let blob = new Blob([fileData]);//, {type: "image/png"});
                 let url = URL.createObjectURL(blob);
-                element.src = url;
+
+                if (await doesImageExist(url))
+                {
+                    let imgNode = document.createElement("img");
+                    imgNode.src = url;
+                    imgNode.alt = filename;
+                    imgNode.className = "chat-image";
+                    element.replaceWith(imgNode);
+
+                    element.onclick = () => {
+                        // open image in new tab
+                        let newTab = window.open(url, '_blank');
+                        newTab.focus();
+                    };
+                }
+                // TODO: Maybe add back in again later will all kinds of text extensions?
+                // else if (filename.endsWith(".txt"))
+                // {
+                //     let aNode = document.createElement("a");
+                //     aNode.href = url;
+                //     aNode.target = "_blank";
+                //     aNode.textContent = `[${filename}]`;
+                //     element.replaceWith(aNode);
+                // }
+                else {
+                    // make a link element that onclick downloads the data
+                    let aNode = document.createElement("a");
+                    aNode.href = url;
+                    aNode.download = filename;
+                    aNode.textContent = `[${filename}]`;
+                    element.replaceWith(aNode);
+                }
             });
-            return `<img src="" alt="${text}" id="${imgId}">`;
+            return `<p id="${imgId}"></p>`;
         }
         else
             return `<a href="${url}" target="_blank">[Image ${text}]</a>`;
