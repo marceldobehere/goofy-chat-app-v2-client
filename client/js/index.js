47a2d4f4e6363ef5f00e96c2bb8dc03df7e2badf
Masl <marcel.z2006@gmail.com>
2024-08-17 02:04:15 +0200

    asking about persistence

--- client/js/index.js
+++ client/js/index.js
@@ -1,8 +1,25 @@
 const sleep = async (time) => await new Promise(r => setTimeout(r, time));
 
+async function persist() {
+    return await navigator.storage && navigator.storage.persist &&
+        navigator.storage.persist();
+}
+
+async function isStoragePersisted() {
+    return await navigator.storage && navigator.storage.persisted &&
+        navigator.storage.persisted();
+}
+
 async function init()
 {
     try {
+        await setStatus("Init");
+        await persist();
+        if (await isStoragePersisted())
+            console.log("> Storage is persisted.");
+        else
+            console.warn("> Storage is not persisted!");
+
         await setStatus("Init Lib")
         console.log("> Initializing client lib...");
         await initClientLib();
